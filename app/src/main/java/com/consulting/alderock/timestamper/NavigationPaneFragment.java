33e401621bb5de3d0057d8ef166d3b4821ff297f
Erik Ahlberg <Erik.M.Ahlberg@gmail.com>
2017-02-21 18:46:34 +0100

    Added Time Stamping activity.
      - Added skeleton Time Stamping activity with centre stamping button.
      - Included skeleton navigation pane.

---
+++ app/src/main/java/com/consulting/alderock/timestamper/NavigationPaneFragment.java
@@ -0,0 +1,31 @@
+package com.consulting.alderock.timestamper;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Erik on 2017-01-21.
+ */
+public class NavigationPaneFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.navigation_pane_fragment,
+                container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Bundle bundle = getArguments();
+
+        if (bundle != null) {
+        }
+    }
+}
